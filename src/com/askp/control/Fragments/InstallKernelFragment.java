72804a04b111727e30c4fef48d0b960c48ea3948
Grarak <williye97@gmail.com>
2013-12-25 23:15:15 +0100

    remove unused import
    Signed-off-by: Grarak <williye97@gmail.com>

--- src/com/askp/control/Fragments/InstallKernelFragment.java
+++ src/com/askp/control/Fragments/InstallKernelFragment.java
@@ -15,7 +15,6 @@ import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.Fragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
