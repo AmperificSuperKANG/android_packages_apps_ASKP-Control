6a4913e4168d6731f68722698e5078dff06f151a
Willi Ye <williye97@gmail.com>
2014-01-03 15:09:52 +0100

    license ASKP Control as GPLv3
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- src/com/askp/control/fragments/NewsFragment.java
+++ src/com/askp/control/fragments/NewsFragment.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2014 AmperificSuperKANG Project
+ *
+ * This file is part of ASKP Control.
+ *
+ * ASKP Control is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ASKP Control is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ASKP Control.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.askp.control.fragments;
 
 import com.askp.control.R;
--- src/com/askp/control/utils/IoAlgorithmValues.java
+++ src/com/askp/control/utils/IoAlgorithmValues.java
@@ -1,19 +1,20 @@
 /*
- * Copyright (C) 2013 AmperificSuperKANG Project
+ * Copyright (C) 2014 AmperificSuperKANG Project
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 3 as published by the Free Software Foundation.
+ * This file is part of ASKP Control.
  *
- * This program is distributed in the hope that it will be useful,
+ * ASKP Control is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ASKP Control is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA  02110-1301, USA.
+ * along with ASKP Control.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.askp.control.utils;
